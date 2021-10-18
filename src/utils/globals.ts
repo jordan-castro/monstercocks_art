be3b74e8e3dbca782988b5480f95852fc24461c8
James Garfield <jordan@grupojvm.com>
2021-10-18 16:33:30 -0500

    Changed to constant style

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -1,2 +1,2 @@
-export const contractAddress = "0x8F8b452E4B9951C7eAB9ebb1Fc897f9F0d597f15";
+export const CONTRACT_ADDRESS = "0x8F8b452E4B9951C7eAB9ebb1Fc897f9F0d597f15";
 export const MCK_BASE = "https://monstercocks.art/";
