64621697e97e7fa022221840a24ed3d89ab836ca
James Garfield <jordan@grupojvm.com>
2021-10-19 18:43:19 -0500

    convert CockCard to class for image loading.

--- src/view/widgets/cocks/cock_card.tsx
+++ src/view/widgets/cocks/cock_card.tsx
@@ -1,46 +1,63 @@
 import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import React from "react";
 import MonsterCock from "../../../models/cock";
 import './cock.css';
+import Skeleton from '@mui/material/Skeleton';
 
 /**
  * Una carta para un Cock.
- * 
- * @param cock
- * MonsterCock, el cock que va en la carta. 
  */
-export function CockCard(props: { cock?: MonsterCock, main?: boolean }) {
-    const { cock, main } = props;
-
-    const cName = props.main !== undefined
-        ? props.main
+export class CockCard extends React.Component<{ cock?: MonsterCock, main?: boolean }, { loading: boolean }> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+        };
+    }
+    cName = this.props.main !== undefined
+        ? this.props.main
             ? 'cock-main' : 'cock-def'
         : 'cock-def';
 
-    return (
-        <div className={cName}>
-            <Card>
-                {
-                    cock !== undefined ?
-                        <CardMedia
-                            component="img"
-                            // height="800"
-                            image={cock.image}
-                        />
-                        : null
-                }
-                {
-                    main !== undefined && !main ?
-                        <CardContent>
-                            {
-                                cock !== undefined ?
-                                    <Typography variant="h5" component="div">
-                                        {cock.name}
-                                    </Typography> : null
-                            }
-                        </CardContent>
-                        : null
-                }
-            </Card>
-        </div>
-    );
+    img() {
+        return this.state.loading ? <Skeleton
+            sx={{ bgcolor: 'grey.900' }}
+            variant="rectangular"
+            width={'1000'}
+            height={800}
+        /> : <CardMedia
+            component='img'
+            // height="800"
+            image={this.props.cock?.image}
+            onLoad={function () {
+                console.log("Cargado");
+            }}
+        />;
+    }
+
+    render() {
+        return (
+            <div className={this.cName}>
+                <Card>
+                    {
+                        this.props.cock !== undefined ?
+                            this.img()
+                            : null
+                    }
+                    {
+                        this.props.main !== undefined && !this.props.main ?
+                            <CardContent>
+                                {
+                                    this.props.cock !== undefined ?
+                                        <Typography variant="h5" component="div">
+                                            {this.props.cock.name}
+                                        </Typography> : null
+                                }
+                            </CardContent>
+                            : null
+                    }
+                </Card>
+            </div>
+        );
+    }
 }
