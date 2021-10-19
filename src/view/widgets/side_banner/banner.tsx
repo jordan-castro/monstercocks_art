dfee2b0fe83b1794ba6a804c6119b3c469530b87
James Garfield <jordan@grupojvm.com>
2021-10-19 18:43:40 -0500

    Edit with banner css and layouting

--- src/view/widgets/side_banner/banner.tsx
+++ src/view/widgets/side_banner/banner.tsx
@@ -3,6 +3,7 @@ import { CONTRACT_ADDRESS } from "../../../utils/globals";
 import './banner.css';
 import shortenString, { shortenAddress } from '../../../utils/shorten_string';
 import { Link } from "@mui/material";
+import { StoreButton, StoreButtons } from "../store_buttons/store_buttons";
 
 const H = (props: { title: string, value, onClick?, blueify?: boolean }) =>
     <div className="banner-child">
@@ -39,39 +40,50 @@ export function Banner(props: { owner: string, cock: MonsterCock }) {
     const { owner, cock } = props;
 
     return (
-        <div className="banner">
-            <H
-                title={'Name'}
-                value={cock.name}
-            />
-            <H
-                title={'Id'}
-                value={cock.id}
-            />
-            <Link onClick={() => window.open(cock.uri)}>
-                <H
-                    title={'Token URI'}
-                    value={cock.uri}
-                />
-            </Link>
-            <Link onClick={() => window.open(cock.image)}>
-                <H
-                    title={'Image source'}
-                    value={cock.image}
-                />
-            </Link>
-            <H
-                title={'Owner'}
-                value={shortenAddress(owner)}
-            />
-            <H
-                title={'Chain'}
-                value={'Polygon'}
-            />
-            <H
-                title={'Address'}
-                value={shortenAddress(CONTRACT_ADDRESS)}
-            />
-        </div>
+        <>
+            <div className="banner">
+                <h1>Token Data</h1>
+                <div className="banner-data">
+                    <H
+                        title={'Name'}
+                        value={cock.name}
+                    />
+                    <H
+                        title={'Id'}
+                        value={cock.id}
+                    />
+                    <Link onClick={() => window.open(cock.uri)}>
+                        <H
+                            title={'Token URI'}
+                            value={cock.uri}
+                        />
+                    </Link>
+                    <Link onClick={() => window.open(cock.image)}>
+                        <H
+                            title={'Image source'}
+                            value={cock.image}
+                        />
+                    </Link>
+                    <H
+                        title={'Owner'}
+                        value={shortenAddress(owner)}
+                    />
+                    <H
+                        title={'Chain'}
+                        value={'Polygon'}
+                    />
+                    <H
+                        title={'Address'}
+                        value={shortenAddress(CONTRACT_ADDRESS)}
+                    />
+                </div>
+                <div className="banner-button">
+                    <StoreButton
+                        type={StoreButtons.opensea}
+                        cock={cock}
+                    />
+                </div>
+            </div>
+        </>
     );
 }
