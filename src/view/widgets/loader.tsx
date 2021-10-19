a550d941be64da54b9009be8db30da08e9ecca35
James Garfield <jordan@grupojvm.com>
2021-10-19 18:43:03 -0500

    MUI loader

--- src/view/widgets/loader.tsx
+++ src/view/widgets/loader.tsx
@@ -1,1 +1,9 @@
-export const Loader = () => <div>Loading...</div>;
+import { CircularProgress } from "@mui/material";
+import Box from '@mui/material/Box';
+
+export const Loader = () =>
+    <div className="center-both">
+        <Box>
+            <CircularProgress />
+        </Box>
+    </div>
