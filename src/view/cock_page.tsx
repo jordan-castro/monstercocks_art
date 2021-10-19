233964aa073fc3d90fd7d8c547e4fe8bfc3a532c
James Garfield <jordan@grupojvm.com>
2021-10-18 20:05:48 -0500

    Started cock page builder

--- src/view/cock_page.tsx
+++ src/view/cock_page.tsx
@@ -1,1 +1,71 @@
-export {};
+import React from "react";
+import { fetchCock } from "../controller/fetch_cocks";
+import MonsterCock from "../models/cock";
+import AttributesWidget from "./widgets/attributes/attributes";
+import { CompilerSuggestion } from "./widgets/compiler_suggestion";
+import { Loader } from "./widgets/loader";
+import { useParams } from 'react-router-dom';
+
+export function CockPage() {
+    var { id } = useParams();
+
+    return (
+        <CockPageBuilder id={id} />
+    );
+}
+
+class CockPageBuilder extends React.Component<{ id?: number }, { cock?: MonsterCock, loading: boolean, error: boolean }> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cock: props.cock,
+            loading: true,
+            error: false,
+        };
+    }
+
+    compilerSuggestion() {
+        this.setState({
+            error: true,
+            loading: false,
+        });
+    }
+
+    componentDidMount() {
+        // Chequea si cock no existe
+        if (this.state.cock === undefined) {
+            // Chequea para ID
+            if (this.props.id !== undefined) {
+                // Carga el cock
+                fetchCock(this.props.id).then((cock) => {
+                    if (cock === false) {
+                        // Suggestion de compiler
+                        this.compilerSuggestion();
+                    } else {
+                        // Tenemos un cock!
+                        this.setState({
+                            loading: false,
+                            error: false,
+                            cock: cock
+                        });
+                    }
+                }
+                ).catch((error) => this.compilerSuggestion());
+            } else {
+                this.compilerSuggestion();
+            }
+        }
+    }
+
+    render() {
+        return (
+            this.state.loading
+                ? <Loader />
+                : this.state.error
+                    ? <CompilerSuggestion />
+                    : <AttributesWidget
+                        attributes={(this.state.cock as MonsterCock).attributes}
+                    />
+        );
+    }
+}
