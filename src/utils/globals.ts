5293fab08e5321bbc3fa6a14adb181e66459f964
James Garfield <jordan@grupojvm.com>
2021-10-19 15:45:56 -0500

    Added Open Sea image urls

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -1,2 +1,6 @@
 export const CONTRACT_ADDRESS = "0x8F8b452E4B9951C7eAB9ebb1Fc897f9F0d597f15";
 export const MCK_BASE = "https://monstercocks.art/";
+
+// Imagenes de OPENSEA
+export const OPENSEA_LIGHT_BANNER = "https://storage.googleapis.com/opensea-static/Logomark/Badge%20-%20Available%20On%20-%20Light.png"
+export const OPENSEA_DARK_BANNER = "https://storage.googleapis.com/opensea-static/Logomark/Badge%20-%20Available%20On%20-%20Dark.png"
