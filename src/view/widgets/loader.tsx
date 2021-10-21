73591ad65e8d1f5e5fa34aaa40531edca294bf4a
James Garfield <jordan@grupojvm.com>
2021-10-20 23:46:20 -0500

    Customized loader with props.

--- src/view/widgets/loader.tsx
+++ src/view/widgets/loader.tsx
@@ -1,9 +1,20 @@
 import { CircularProgress } from "@mui/material";
 import Box from '@mui/material/Box';
 
-export const Loader = () =>
-    <div className="center-both">
-        <Box>
-            <CircularProgress />
-        </Box>
-    </div>
+export const Loader = (props) => {
+    let { centerType } = props;
+
+    if (centerType === undefined) {
+        centerType = 'center-both';
+    }
+
+    return (
+        <div
+            className={centerType}
+            style={props.style}>
+            <Box>
+                <CircularProgress />
+            </Box>
+        </div>
+    );
+}
