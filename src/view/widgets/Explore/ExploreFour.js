2a80feae36decb0fdabdb60eabae019337cb625e
James Garfield <jordan@grupojvm.com>
2021-10-27 17:57:59 -0500

    view/widgets/Explore/ExploreFour: Poni un nuevo component para el explorer de 4 y does

--- src/view/widgets/Explore/ExploreFour.js
+++ src/view/widgets/Explore/ExploreFour.js
@@ -1,4 +1,116 @@
 import React, { Component } from 'react';
+import { fetchOwner } from '../../../controller/fetch_owner';
+import { fetchCreatorTransaction } from '../../../controller/fetch_transactions';
+import { shortenAddress } from '../../../utils/shorten_string';
+import { polygonUrl } from '../../../utils/url_builder';
+
+export class CockCardExploreFour extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            owner: null,
+            creator: null
+        };
+    }
+
+    /**
+     * @dev fetch owner details
+     */
+    async grabOwner() {
+        // Busca detalles del dueño
+        let owner = await fetchOwner(this.props.cock.id);
+        // Chequea si el dueño existe
+        if (owner) {
+            this.setState({
+                owner: owner
+            });
+        }
+    }
+
+    /**
+     * @dev Busca detailes de creador.
+     */
+    async grabCreator() {
+        // Busca creador
+        let creator = await fetchCreatorTransaction(this.props.cock.id);
+        // Chequea si el creador existe
+        if (creator) {
+            this.setState({
+                creator: creator
+            });
+        }
+    }
+
+    componentDidMount() {
+        this.grabOwner();
+        this.grabCreator();
+    }
+
+    render() {
+        const { cock } = this.props;
+
+        return (
+            <div key={`exf_${this.props.idx}`} className="col-12 col-sm-6 col-lg-3 item" style={{
+                display: "block"
+            }}>
+                <div className="card">
+                    <div className="image-over">
+                        <a href={`/cock/${cock.id}`}>
+                            <img className="card-img-top" src={cock.image} alt="" />
+                        </a>
+                    </div>
+                    {/* Card Caption */}
+                    <div className="card-caption col-12 p-0">
+                        {/* Card Body */}
+                        <div className="card-body">
+                            <a href={`/cock/${cock.id}`}>
+                                <h5 className="mb-0">{cock.name}</h5>
+                            </a>
+                            <div className="seller d-flex align-items-center my-2">
+                                <span>Owned By</span>
+                                {
+                                    // Chequea si hay owner en el estado,
+                                    // Si no hay entonces muestra "Loading"
+                                    // Si hay entonces muestra el owner.address
+                                    this.state.owner ?
+                                        (
+                                            <a href="/author">
+                                                <h6 className="ml-2 mb-0">
+                                                    {shortenAddress(this.state.owner.address)}
+                                                </h6>
+                                            </a>
+                                        ) : (
+                                            <h6 className="ml-2 mb-0">
+                                                Loading
+                                            </h6>
+                                        )
+                                }
+                            </div>
+                            <div className="seller d-flex align-items-center my-1">
+                                {
+                                    // Chequea si hay owner en el estado,
+                                    // Si no hay entonces muestra null
+                                    this.state.creator ?
+                                        (
+                                            <>
+                                                <span>Created On</span>
+                                                <a href={polygonUrl({ tx: this.state.creator.hash })} target="_new">
+                                                    <h6 className="ml-2 mb-0">
+                                                        {this.state.creator.date.toLocaleDateString().replace(/\//g, "-")}
+                                                    </h6>
+                                                </a>
+                                            </>
+                                        ) : null
+                                }
+                            </div>
+                            <a className="btn btn-bordered-white btn-smaller mt-3" href={`/cock/${cock.id}`}><i className="icon-eye mr-2" />View</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
 const initData = {
     pre_heading: "Explore",
