ee6caa3d55d8a7d91a7e5e9a6c5beafd9b5bc929
James Garfield <jordan@grupojvm.com>
2021-10-28 19:59:01 -0500

    view/widgets/Signup/Signup: Podemos actualizar nuestro cuenta ahora.

--- src/view/widgets/Signup/Signup.js
+++ src/view/widgets/Signup/Signup.js
@@ -1,55 +1,44 @@
 import React, { Component } from 'react';
-import connectWallet from '../../../utils/connect_wallet';
+import connectWallet, { checkWallet } from '../../../utils/connect_wallet';
+import swal from '@sweetalert/with-react';
+import { createAuthor } from '../../../controller/fetch_author';
 
 const initData = {
-    pre_heading: "Signup",
-    heading: "Create an Account",
-    content: "Your account is created based on your connected wallet address."
+    pre_heading: "Edit",
+    heading: "Edit your Account",
+    content: "You can edit your account with a custom nickname, avatar, and short description!"
 }
-
-const socialData = [
-    {
-        id: "1",
-        link: "facebook",
-        icon: "fab fa-facebook-f"
-    },
-    {
-        id: "2",
-        link: "twitter",
-        icon: "fab fa-twitter"
-    },
-    {
-        id: "3",
-        link: "google-plus",
-        icon: "fab fa-google-plus-g"
-    }
-]
 
 class Signup extends Component {
     constructor(props) {
         super(props);
         this.state = {
             initData: initData,
-            hasClickedPrivacy: false,
             address: "",
+            name: "",
+            about: "",
         };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     // Chequea web3 esta connectado
     async checkWeb3() {
-        if (window.web3.isConnected()) {
-            console.log(window.web3.eth.accounts[0]);
-            // Hacemos la vaina de address en el state
+        // Chequea si esta conectado por el sessionStorage.
+        if (checkWallet()) {
+            // Si esta connectado entonces usa el wallet address de connectWallet()
             this.setState({
-                address: window.web3.eth.accounts[0]
-            })
-        } else {
-            // Connecta web3 y hace la vaina de address en el state
-            await connectWallet();
-            this.setState({
-                address: window.web3.eth.accounts[0]
+                address: await connectWallet()
             });
-            // return false;
+        } else {
+            // Muestra mensaje de connectar
+            swal({
+                title: "Connect Wallet",
+                text: "Please connect your wallet to continue.",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            });
         }
     }
 
@@ -57,6 +46,44 @@ class Signup extends Component {
         // Chequea web3
         this.checkWeb3();
     }
+
+    // Handle el submit del form
+    async handleSubmit(event) {
+        event.preventDefault();
+        let data = {
+            name: this.state.name,
+            address: this.state.address,
+            about: this.state.about,
+        };
+        // Manda al server
+        let result = await createAuthor(
+            data.address,
+            data.name,
+            data.about
+        );
+        console.log(result);
+
+        // Chequea result
+        if (result === false) {
+            // Si no se pudo crear el author
+            swal({
+                title: "Error",
+                text: "Something went wrong, please try again.",
+                icon: "error",
+                buttons: true,
+                dangerMode: true,
+            });
+        } else {
+            // Creamos el author
+            swal({
+                title: "Success",
+                text: "Your account has been updated!",
+                icon: "success",
+                buttons: true,
+            });
+        }
+    }
+
     render() {
         return (
             <section className="author-area">
@@ -70,46 +97,77 @@ class Signup extends Component {
                                 <p>{this.state.initData.content}</p>
                             </div>
                             {/* Item Form */}
-                            <form className="item-form card no-hover">
+                            <form className="item-form card no-hover"
+                                onSubmit={this.handleSubmit}
+                                method='post'>
                                 <div className="row">
                                     <div className="col-12">
                                         <div className="form-group mt-3">
-                                            <input type="text" className="form-control" name="name" placeholder="Enter your Name" required="required" />
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                name="address"
+                                                readOnly
+                                                value={this.state.address}
+                                            />
                                         </div>
                                     </div>
                                     <div className="col-12">
                                         <div className="form-group mt-3">
-                                            <input type="email" className="form-control" name="email" placeholder="Enter your Email" required="required" />
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                name="name"
+                                                placeholder="Enter your Name"
+                                                required="required"
+                                                value={this.state.name}
+                                                onChange={(e) => this.setState({ name: e.target.value })}
+                                            />
                                         </div>
                                     </div>
                                     {/* <div className="col-12">
                                         <div className="form-group mt-3">
-                                            <input type="password" className="form-control" name="password" placeholder="Enter your Password" required="required" />
+                                            <input 
+                                            type="email" 
+                                            className="form-control" 
+                                            name="email" 
+                                            placeholder="Enter your Email" 
+                                            required="required" 
+                                            value={this.state.email}
+                                            onChange={(e) => this.setState({email: e.target.value})}
+                                            />
                                         </div>
                                     </div> */}
                                     <div className="col-12">
                                         <div className="form-group mt-3">
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                name="about"
+                                                placeholder="Short description"
+                                                maxLength="120"
+                                                value={this.state.about}
+                                                onChange={(e) => this.setState({ about: e.target.value })}
+                                            />
+                                        </div>
+                                    </div>
+                                    {/* <div className="col-12">
+                                        <div className="form-group mt-3">
                                             <div className="form-check form-check-inline">
-                                                <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" defaultValue="option1" />
+                                                <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" defaultValue="option1" required="required" />
                                                 <label 
                                                 className="form-check-label" 
                                                 htmlFor="inlineRadio1" 
-                                                onClick={() => {
-                                                    this.setState({
-                                                        hasClickedPrivacy: true
-                                                    })
-                                                }}>
+                                                >
                                                     I agree to <a href="#">Privacy Policy</a></label>
                                             </div>
                                         </div>
-                                    </div>
+                                    </div> */}
                                     <div className="col-12">
-                                        <button className="btn w-100 mt-3 mt-sm-4" type="submit" onClick={() => {
-                                            if (!this.state.hasClickedPrivacy) {
-                                                // Muestra un dialog customizado
-                                                // alert("You must agree to the privacy policy to continue");
-                                            }
-                                        }}>Sign Up</button>
+                                        <button
+                                            className="btn w-100 mt-3 mt-sm-4"
+                                            type="submit"
+                                        >Sign Up</button>
                                     </div>
                                     {/* <div className="col-12">
                                         <span className="d-block text-center mt-4">Already have an account? <a href="/login">Login</a></span>
