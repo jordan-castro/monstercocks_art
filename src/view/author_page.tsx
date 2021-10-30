e33d0ef7db0eb875775f13513a90d76d6527bbf8
James Garfield <jordan@grupojvm.com>
2021-10-29 19:06:43 -0500

    view/author_page: Changed to functional constructor

--- src/view/author_page.tsx
+++ src/view/author_page.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import AuthorProfile from './widgets/Author/Author';
 import Breadcrumb from "./widgets/Breadcrumb/Breadcrumb";
 import Footer from "./widgets/Footer/Footer";
@@ -6,23 +5,23 @@ import Header from "./widgets/Header/Header";
 import ModalMenu from "./widgets/Modal/ModalMenu";
 import ModalSearch from "./widgets/Modal/ModalSearch";
 import Scrollup from "./widgets/Scrollup/Scrollup";
+import { useParams } from 'react-router-dom';
 
-export default class AuthorPage extends React.Component<{}, {}> {
-    constructor(props) {
-        super(props);
-    }
+const AuthorPage = (props) => {
+    // Chequea el address de la url con useParams
+    const { address } = useParams();
 
-    render() {
-        return (
-            <div className="main">
-                <Header />
-                <Breadcrumb title="Author Profile" subpage="Pages" page="Author" />
-                <AuthorProfile />
-                <Footer />
-                <ModalSearch />
-                <ModalMenu />
-                <Scrollup />
-            </div>
-        );
-    }
+    return (
+        <div className="main">
+            <Header />
+            <Breadcrumb title="Author Profile" subpage="Pages" page="Author" />
+            <AuthorProfile address={address} />
+            <Footer />
+            <ModalSearch />
+            <ModalMenu />
+            <Scrollup />
+        </div>
+    );
 }
+
+export default AuthorPage;
