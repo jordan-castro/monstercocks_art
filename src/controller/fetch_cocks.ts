588cc06876a3b9cb5fae70c2841446726c43a5ab
James Garfield <jordan@grupojvm.com>
2021-11-02 14:25:10 -0500

    controller/fetch_cocks: Fecth most recent cocks.

--- src/controller/fetch_cocks.ts
+++ src/controller/fetch_cocks.ts
@@ -143,7 +143,39 @@ export async function fetchRandomCocks(): Promise<MonsterCock[]> {
     let cocks: MonsterCock[] = [];
 
     // Ahora creamos los cocks! Usando parse.
-    for (var cock of (response.data as any).cocks) {
+    for (var cock of (response.data as any)) {
+        // Crea y pon el cock
+        const parsedCock = parseCock(cock);
+        cocks.push(parsedCock);
+    }
+
+    return cocks;
+}
+
+/**
+ * Lee los cocks mas reciente. Buscamos en el api con el parametro `q=mostrecent`.
+ * 
+ * @returns `Promise<MonsterCock[]>`
+ */
+export async function fetchMostRecentCocks(): Promise<MonsterCock[]> { 
+    // Busca los cocks
+    const response = await valid_http(`${MCK_BASE}${API_COCK_GATEWAY}`, {
+        params: {
+            p: API_KEY,
+            q: 'mostrecent'
+        }
+    });
+
+    // Chequea que funcione
+    if (response === false) {
+        return [];
+    }
+
+    // Crea el array
+    let cocks: MonsterCock[] = [];
+
+    // Ahora creamos los cocks! Usando parse.
+    for (var cock of (response.data as any)) {
         // Crea y pon el cock
         const parsedCock = parseCock(cock);
         cocks.push(parsedCock);
